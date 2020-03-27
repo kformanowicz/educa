f7a7dbc09992ba709426141d1bfdffd177d39bed
Krzysztof Formanowicz <formanowicz.k@gmail.com>
2020-03-27 14:06:32 +0400

    added reordering

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     url(r'^module/(?P<module_id>\d+)/content/(?P<model_name>\w+)/create/$', views.ContentCreateUpdateView.as_view(), name='module_content_create'),
     url(r'^module/(?P<module_id>\d+)/content/(?P<model_name>\w+)/(?P<id>\d+)/$', views.ContentCreateUpdateView.as_view(), name='module_content_update'),
     url(r'^module/(?P<module_id>\d+)/$', views.ModuleContentListView.as_view(), name='module_content_list'),
-    url(r'^content/(?P<id>\d+)/delete/$', views.ContentDeleteView.as_view(), name='module_content_delete')
+    url(r'^module/order/$', views.ModuleOrderView.as_view(), name='module_order'),
+    url(r'^content/(?P<id>\d+)/delete/$', views.ContentDeleteView.as_view(), name='module_content_delete'),
+    url(r'^content/order/$', views.ContentOrderView.as_view(), name='content_order')
 ]
